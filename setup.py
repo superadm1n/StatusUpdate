c5baeadc867312484ca7c798b82327d3f3c72e66
Kyle Kowalczyk <kowalkyl@gmail.com>
2017-12-12 17:57:54 -0600

    Added MIT license and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='StatusUpdate',
+    version='1.00',
+    packages=['StatusUpdate'],
+    url='',
+    license='MIT',
+    author='Kyle Kowalczyk',
+    author_email='kowalkyl@gmail.com',
+    description='Provide interface to create a status bar or a spinning status circle',
+    install_requires=[]
+)
